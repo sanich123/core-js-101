3ce83c0e9cf9f16235c53790b575364cd45a396c
sanich123 <aovoronin.piano@gmail.com>
2022-11-25 16:45:46 +0300

    feat: work on date problems

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,8 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  return new Date(date.getFullYear(), 1, 29).getDate() === 29;
 }
 
 
@@ -73,8 +73,30 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const diff = Math.abs(startDate - endDate);
+  const millisecsInHour = 3600000;
+  const millisecsInMinute = 60000;
+  const millisecsInSecond = 1000;
+
+  const hours = Math.floor(diff / millisecsInHour);
+  const hoursInMillisecs = hours * millisecsInHour;
+  const minutes = Math.floor((diff - hoursInMillisecs) / millisecsInMinute);
+  const minutesInMillisecs = minutes * millisecsInMinute;
+  const seconds = Math.floor((diff - hoursInMillisecs - minutesInMillisecs) / millisecsInSecond);
+  const secondsInMillisecs = seconds * millisecsInSecond;
+  const millisecs = diff - hoursInMillisecs - minutesInMillisecs - secondsInMillisecs;
+  const zeroAdder = (date) => {
+    if (date < 100 && date >= 10) {
+      return `0${date}`;
+    }
+    if (date < 10) {
+      return `00${date}`;
+    }
+    return date;
+  };
+
+  return `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}.${zeroAdder(millisecs)}`;
 }
 
 
